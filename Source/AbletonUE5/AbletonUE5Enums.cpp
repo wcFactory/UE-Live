d7b0b859fc7313efbffcf677489fdfc93eaf4b28
wcFactory <159144948+wcFactory@users.noreply.github.com>
2024-06-19 13:36:16 +0200

    Add CPP to project

---
+++ Source/AbletonUE5/AbletonUE5Enums.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbletonUE5Enums.h"
+
+AbletonUE5Enums::AbletonUE5Enums()
+{
+}
+
+AbletonUE5Enums::~AbletonUE5Enums()
+{
+}
---
+++ Source/AbletonUE5/AbletonUE5Enums.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class ABLETONUE5_API AbletonUE5Enums
+{
+public:
+	AbletonUE5Enums();
+	~AbletonUE5Enums();
+};
